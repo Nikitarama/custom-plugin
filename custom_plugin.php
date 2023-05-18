00f393f2b9bf9aef69290eb37b401dbc2dfabee9
Nikitarama <Nikita@crazyscientist996@gmail.com>
2023-05-18 16:33:41 +0200

    first

---
+++ custom_plugin.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Plugin Name:       Custom_Plugin
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Nikita Ramawoothar
+ 
+ */
+
+ ?>
+
+ add_filter( 'show_admin_bar', '__return_false' );
+
+ global $wp_query;
+$total = $wp_query->max_num_pages;
+// only bother with the rest if we have more than 1 page!
+if ( $total > 1 )  {
+     // get the current page
+     if ( !$current_page = get_query_var('paged') )
+          $current_page = 1;
+     // structure of "format" depends on whether we're using pretty permalinks
+     $format = empty( get_option('permalink_structure') ) ? '&page=%#%' : 'page/%#%/';
+     echo paginate_links(array(
+          'base' => get_pagenum_link(1) . '%_%',
+          'format' => $format,
+          'current' => $current_page,
+          'total' => $total,
+          'mid_size' => 4,
+          'type' => 'list'
+     ));
+}
